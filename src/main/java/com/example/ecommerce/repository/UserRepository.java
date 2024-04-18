551b0cf540be9ad41d19e77ba456582c6b1efb26
문정환 <70272622+bazzi2548@users.noreply.github.com>
2024-04-18 20:47:33 +0900

    로그인 검증 로직 구현

--- src/main/java/com/example/ecommerce/repository/UserRepository.java
+++ src/main/java/com/example/ecommerce/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
